422b9bbc4d7e69415cfb1321f15573c81933d2f0
Roman2508 <2508romaa@gmail.com>
2025-05-02 12:35:51 +0300

    build.sh

--- build.sh
+++ build.sh
@@ -11,9 +11,19 @@ curl -sSL https://install.python-poetry.org | python3 -
 npm install
 npm run build
 
-# Python-залежності
+# Python-залежності (recommendation)
 cd src/modules/recommendation/python
 poetry install
 
+# Зберігаємо шлях до Python з poetry-середовища
+export PYTHON_PATH_REC=$(poetry env info -p)/bin/python
+
+# Python-залежності (nlp)
 cd ../../nlp/python
 poetry install
+
+export PYTHON_PATH_NLP=$(poetry env info -p)/bin/python
+
+# (Опціонально) записати ці шляхи у файл .env або передати у NestJS
+echo "PYTHON_PATH_REC=$PYTHON_PATH_REC" >> /etc/environment
+echo "PYTHON_PATH_NLP=$PYTHON_PATH_NLP" >> /etc/environment
