33f668385045cfb5b49f911ce2296aca76170019
Roman2508 <2508romaa@gmail.com>
2025-05-04 19:03:17 +0300

    update build.sh 2

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-# Вихід з виконання при помилці будь-якої команди
-set -e
+# # Вихід з виконання при помилці будь-якої команди
+# set -e
 
-# Створюємо venv, якщо його ще немає
-if [ ! -d "venv" ]; then
-  python -m venv venv
-fi
+# # Створюємо venv, якщо його ще немає
+# if [ ! -d "venv" ]; then
+#   python -m venv venv
+# fi
 
+python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
